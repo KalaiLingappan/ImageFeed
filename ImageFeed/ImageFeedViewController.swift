083eeca14c39747cc3fb8fd6187058b6a34e2437
Kalaiprabha <klingappan@aligntech.com>
2022-02-12 20:48:00 +0530

    first commit

---
+++ ImageFeed/ImageFeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Kalaiprabbha L on 12/02/22.
+//
+
+import UIKit
+
+class ImageFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
